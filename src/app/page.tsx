6f0c2d3a1dd6f966236221ef7a57c5a56e5e2573
Ratul Anjum <206848890+dev-ratul-anjum@users.noreply.github.com>
2026-02-15 16:39:20 +0600

    Refactor HomePage component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
 
-const HomePage = () => {
+export default function HomePage() {
   return (
     <div>
       <Link href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/set-cookie`}>
         Set Cookie
       </Link>
-     <h1>Anchor > </h1>
-      <a href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/set-cookie`}>
-  Set Cookie
-</a>
 
+      <h1>Anchor {" > "}</h1>
+
+      <a href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/set-cookie`}>
+        Set Cookie
+      </a>
     </div>
   );
-};
-
-export default HomePage;
+}
