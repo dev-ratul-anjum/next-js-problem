c7b59d8ea480adc6185d53f703d6632f4be077f3
Ratul Anjum <206848890+dev-ratul-anjum@users.noreply.github.com>
2026-02-15 16:33:31 +0600

    Add anchor tag for setting cookie link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ const HomePage = () => {
       <Link href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/set-cookie`}>
         Set Cookie
       </Link>
+     <h1>Anchor > </h1>
+      <a href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/set-cookie`}>
+  Set Cookie
+</a>
+
     </div>
   );
 };
