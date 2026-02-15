9a17219c4de91814152996b8a801b2938a2183c6
Ratul Anjum <206848890+dev-ratul-anjum@users.noreply.github.com>
2026-02-15 16:29:26 +0600

    Configure CORS with dynamic origin and credentials

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ if(process.env.NODE_ENV === "production"){
       app.set("trust proxy", 1);
 }
 
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.FRONTEND_REDIRECT_URL,
+    credentials: true,
+  })
+);
+
 app.use(cookieParser("cookie-secret"))
 
 app.get("/set-cookie", (req, res) => {
