a31f393969961d7b12fa57d0216fe7bd17726052
dev-ratul-anjum <206848890+dev-ratul-anjum@users.noreply.github.com>
2026-02-16 11:30:20 +0600

    remove signed true

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get("/set-cookie", (req, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    signed: true,
+    // signed: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
   });
 
